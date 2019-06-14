4d91405d38c9231f1a93d7d6ec27419b66938fdb
manyce400 <fallon12>
2019-06-13 21:20:35 -0400

    Adding full working integration with Hibernate and MySQL

---
+++ imani-cash-domain/src/main/java/com/imani/cash/domain/property/repository/IPropertyManagerServiceProviderRepository.java
@@ -0,0 +1,14 @@
+package com.imani.cash.domain.property.repository;
+
+import com.imani.cash.domain.property.PropertyManagerServiceProvider;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author manyce400
+ */
+public interface IPropertyManagerServiceProviderRepository extends JpaRepository<PropertyManagerServiceProvider, Long> {
+
+
+
+
+}
---
+++ imani-cash-domain/src/main/java/com/imani/cash/domain/serviceprovider/repository/IServiceProviderRepository.java
@@ -0,0 +1,11 @@
+package com.imani.cash.domain.serviceprovider.repository;
+
+import com.imani.cash.domain.serviceprovider.ServiceProvider;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author manyce400
+ */
+public interface IServiceProviderRepository extends JpaRepository<ServiceProvider, Long> {
+
+}
